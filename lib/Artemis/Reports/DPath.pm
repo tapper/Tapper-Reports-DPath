b28bcf13d20a18d9d62b655c44024a1836ac8ff7
Steffen Schwigon <steffen.schwigon@amd.com>
2009-07-15 10:59:54 +0200

    Artemis::DPath: introduce dpath caching
    - single dpath per report_id
    - report-dpaths per complete db

--- lib/Artemis/Reports/DPath.pm
+++ lib/Artemis/Reports/DPath.pm
@@ -8,6 +8,8 @@ class Artemis::Reports::DPath is dirty {
         use Text::Balanced 'extract_codeblock';
         use Data::DPath::Path;
         use Data::Dumper;
+        use Cache::FileCache;
+
         use Sub::Exporter -setup => { exports =>           [ 'reportdata' ],
                                       groups  => { all  => [ 'reportdata' ] },
                                     };
@@ -39,6 +41,65 @@ class Artemis::Reports::DPath is dirty {
                 return $condition;
 
         }
+
+        # ===== CACHE =====
+
+        # ----- cache complete Artemis::Reports::DPath queries -----
+
+        sub cache_whole_dpath  {
+                my ($reports_path, $rs_count, $res) = @_;
+
+                my $cache = new Cache::FileCache;
+
+                # we cache on the dpath
+                # but need count to verify and maintain cache validity
+
+                $cache->set( $reports_path, {
+                                             count => $rs_count,
+                                             res   => $res,
+                                            });
+        }
+
+        sub cached_whole_dpath {
+                my ($reports_path, $rs_count) = @_;
+
+                my $cache      = new Cache::FileCache;
+                my $cached_res = $cache->get( $reports_path );
+
+                return undef              if not defined $cached_res;
+                return $cached_res->{res} if $cached_res->{count} == $rs_count;
+
+                # clean up when matching report count changed
+                $cache->remove( $reports_path );
+                return undef;
+        }
+
+        # ----- cache single report dpaths queries -----
+
+        sub _cachekey_single_dpath {
+                my ($path, $reports_id) = @_;
+                $reports_id."::".$path;
+        }
+
+        sub cache_single_dpath {
+                my ($path, $reports_id, $res) = @_;
+
+                my $cache = new Cache::FileCache;
+                $cache->set( _cachekey_single_dpath( $path, $reports_id ),
+                             $res
+                           );
+        }
+
+        sub cached_single_dpath {
+                my ($path, $reports_id) = @_;
+
+                my $cache      = new Cache::FileCache;
+                my $cached_res = $cache->get( _cachekey_single_dpath( $path, $reports_id ));
+
+                return $cached_res;
+        }
+
+        # ===== the query search =====
 
         sub reports_dpath_search($) {
                 my ($reports_path) = @_;
@@ -87,13 +148,35 @@ class Artemis::Reports::DPath is dirty {
                       '+as'     => [ 'suite.name', ],
                      }
                     );
+                my $rs_count = $rs->count();
                 my @res = ();
+
+                # layer 2 cache
+                my $cached_res = cached_whole_dpath( $reports_path, $rs_count );
+                return @$cached_res if defined $cached_res;
+
                 while (my $row = $rs->next)
                 {
+                        my $report_id = $row->id;
+                        # layer 1 cache
+
+                        my $cached_row_res = cached_single_dpath( $path, $report_id );
+
+                        if (defined $cached_row_res) {
+                                push @res, @$cached_row_res;
+                                next;
+                        }
+
                         my $data = _as_data($row);
-                        my @row_res  = $dpath->match ($data);
+                        my @row_res = $dpath->match( $data );
+
+                        cache_single_dpath($path, $report_id, \@row_res);
+
                         push @res, @row_res;
                 }
+
+                cache_whole_dpath($reports_path, $rs_count, \@res);
+
                 return @res;
         }
 
@@ -153,7 +236,7 @@ search query, the second part is a normal L<Data::DPath|Data::DPath>
 expression that matches against the datastructure that is build from
 the DB.
 
-=head1 METHODS and FUNCTIONS
+=head1 API FUNCTIONS
 
 =head2 reports_dpath_search
 
@@ -167,6 +250,25 @@ Alias for reports_dpath_search.
 =head2 reportdata
 
 Alias for reports_dpath_search.
+
+
+=head1 UTILITY FUNCTIONS
+
+=head2 cache_single_dpath
+
+Cache a result for a raw dpath on a report id.
+
+=head2 cached_single_dpath
+
+Return cached result for a raw dpath on a report id.
+
+=head2 cache_whole_dpath
+
+Cache a result for a complete artemis::dpath on all reports.
+
+=head2 cached_whole_dpath
+
+Return cached result for a complete artemis::dpath on all reports.
 
 =head1 AUTHOR
 
